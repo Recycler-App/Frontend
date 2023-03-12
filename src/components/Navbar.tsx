4ca7f477d4254046d12478ee921f3a33c3f30edd
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-03-12 07:11:13 -1100

    Done with the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import recyclericon from '../assets/Vector.png'
 
 function Navbar() {
   const [active, setActive] = useState('nav__menu')
@@ -14,23 +15,35 @@ function Navbar() {
     } else setIcon('nav__toggler')
   }
   return (
-    <nav className='nav'>
-      <p>herdoy</p>
+    <nav className='nav p-2 p-md-4'>
+      <div className='d-flex navicon'>
+        <img src={recyclericon} alt='recyclericon' />
+        <p className='ms-1 iconText'>Recycler</p>
+      </div>
       <ul className={active}>
-        <li className='nav__item'>
-          <Link to='/'>Home</Link>
+        <li className='nav__itemhome text-uppercase'>
+          <Link to='/'>home</Link>
+        </li>
+        <li className='nav__item text-uppercase'>
+          <Link to='/'>about us</Link>
+        </li>
+        <li className='nav__item text-uppercase'>
+          <Link to='/'>gallery</Link>
+        </li>
+        <li className='nav__item text-uppercase'>
+          <Link to='/'>contact us</Link>
         </li>
         <li className='nav__item'>
-          <Link to='/'>Home</Link>
+          <Link to='/'>
+            <button className='buttn p-md-2 p-2 text-uppercase'>login</button>
+          </Link>
         </li>
         <li className='nav__item'>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className='nav__item'>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className='nav__item'>
-          <Link to='/'>Home</Link>
+          <Link to='/'>
+            <button className='buttn p-md-2 p-2 text-uppercase'>
+              register
+            </button>
+          </Link>
         </li>
       </ul>
       <div onClick={navToggle} className={icon}>
