a488156899f8af147b855100244b926435ff828e
Maureen Anyanwu <maureenezzy@gmail.com>
2023-03-13 18:48:38 +0100

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import Logo from "../svg/Logo";
+import {  } from "react-icons/bs"
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter } from "react-icons/ai"
+import { FiPhone } from "react-icons/fi"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Flex bg="linear-gradient(180deg, rgba(15, 169, 88, 0.51) 0%, #0FA958 99.99%, #0FA958 100%)" justify="space-between" p="50px" flexWrap="wrap">
+      <Box w="40%">
+        <Flex mb={5}>
+          <Logo />
+          <Text fontWeight={500} fontSize="25px">
+            {" "}
+            Recycler
+          </Text>
+        </Flex>
+        <Text fontWeight={500} fontSize="20px">
+          Recycler is an innovative and unique platform that has revolutionized
+          the way recycling companies and individuals handle plastic waste
+          materials.
+        </Text>
+      </Box>
 
-export default Footer
+      <Box fontSize="20px" fontWeight={500} color="accent" w="20%">
+        <Text fontSize="24px" fontWeight={700} color="light">
+          SUPPORT
+        </Text>
+        <Text>Contact Us</Text>
+        <Text>Privacy Policy</Text>
+        <Text>Cookies</Text>
+        <Text>Customers</Text>
+        <Text>FAQ</Text>
+      </Box>
+
+      <Box fontSize="20px" fontWeight={500} color="accent" w="20%">
+        <Text fontSize="24px" fontWeight={700} color="light">
+          CONNECT WITH US
+        </Text>
+        <Text display="flex" alignItems="center"><FiPhone/>&nbsp;+234 888 909 6654s</Text>
+        <Text display="flex" alignItems="center"><AiOutlineInstagram/>&nbsp;recycler</Text>
+        <Text display="flex" alignItems="center"><AiOutlineTwitter/>&nbsp;recycler_ng</Text>
+        <Text display="flex" alignItems="center"><AiOutlineFacebook/>&nbsp;Recycler Nigeria</Text>
+        <Text display="flex" alignItems="center"><AiOutlineLinkedin/>&nbsp;Recycler Nigeria</Text>
+      </Box>
+      <Box w="100%" mt={10} textAlign="center">
+        <Text fontSize="16px" fontWeight={500} color="accent" opacity={0.5}>Copyright @ 2023 All rights reserved</Text>
+      </Box>
+    </Flex>
+
+  );
+};
+
+export default Footer;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,38 +1,84 @@
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, IconButton, Image, Text } from "@chakra-ui/react";
+import Bg from "../assets/bg.png"
 import React from "react";
+import Logo from "../svg/Logo";
+import { FaChevronDown } from "react-icons/fa"
 
 function Home() {
   return (
     <>
-      <Box px={10}>
-        <Text fontSize="32px" fontWeight="500" color="primary">We focus on waste recycling and the health of the ecosystem</Text>
-        <Flex alignItems="center" justify="space-between">
-          <Text fontSize="24px" fontWeight="400" w="65%">
-            Recycler is a mission-driven company that is dedicated to raising
-            awareness about the importance of recycling plastics. We believe
-            that recycling plastics is essential in order to protect our planet
-            and reduce our global footprint. Recycler is an innovative recycling
-            platform that helps individuals and businesses across the world
-            recycle plastic waste.
-          </Text>
-          <Image src="./cuate.svg" w="30%"/>
-        </Flex>
-        <Text fontSize="24px" fontWeight="500" mt={3}>
-          Our mission is to reduce plastic waste in the environment and create a
-          more sustainable future. We believe that recycling plastics is
-          essential in order to protect our planet and reduce our global
-          footprint, and effective and efficient way of recycling plastic waste.
-          That’s why we created this platform that offers a variety of services
-          and technologies to make the process of plastic recycling as easy and
-          hassle-free as possible. From our easy-to-use online portal to our
-          comprehensive services and technologies, we make sure that our clients
-          have access to the best possible recycling options.
-        </Text>
+        <Box
+            backgroundImage={`url(${Bg})`}
+            minHeight="calc(100vh - 120px)"
+            backgroundAttachment= "fixed"
+            backgroundPosition= "center"
+            backgroundRepeat= "no-repeat"
+            backgroundSize= "cover"
+            color="light"
+        >
+            <Box w="60%" ml="100px" pt="calc(50vh - 242px)">
+                <Flex alignItems="center" mb={5}>
+                    <Logo />
+                    <Text fontWeight={500} fontSize="32px">
+                    {" "}
+                    Recycler
+                    </Text>
+                </Flex>
+                <Text fontWeight={500} fontSize="48px" mb={5}>“Become an Environmental Hero with Recycler - Transform Plastic Waste into a Sustainable Future!"</Text>
+                <Button bg="primary" color="light" px={10} py={7} borderRadius={0} mt={5}>
+                    GET STARTED
+                </Button>
+            </Box>  
+            <IconButton
+                aria-label="scroll"
+                icon={<FaChevronDown/>}
+                bg="transparent"
+                color="light"
+                fontSize="46px"
+                display="block"
+                mt={10}
+                mx="auto"
+                _hover={{
+                    bg:"transparent"
+                }}
+                onClick={() => window.scrollBy(0, 500)}
+            />
+        </Box>
+        
+        <Box px={10} my={10}>
+            <Text fontSize="32px" fontWeight="500" color="primary">We focus on waste recycling and the health of the ecosystem</Text>
+            <Flex alignItems="center" justify="space-between">
+            <Text fontSize="24px" fontWeight="400" w="65%">
+                Recycler is a mission-driven company that is dedicated to raising
+                awareness about the importance of recycling plastics. We believe
+                that recycling plastics is essential in order to protect our planet
+                and reduce our global footprint. Recycler is an innovative recycling
+                platform that helps individuals and businesses across the world
+                recycle plastic waste.
+            </Text>
+            <Image src="./cuate.svg" w="30%"/>
+            </Flex>
+            <Text fontSize="24px" fontWeight="500" mt={3}>
+            Our mission is to reduce plastic waste in the environment and create a
+            more sustainable future. We believe that recycling plastics is
+            essential in order to protect our planet and reduce our global
+            footprint, and effective and efficient way of recycling plastic waste.
+            That’s why we created this platform that offers a variety of services
+            and technologies to make the process of plastic recycling as easy and
+            hassle-free as possible. From our easy-to-use online portal to our
+            comprehensive services and technologies, we make sure that our clients
+            have access to the best possible recycling options.
+            </Text>
 
-        <Button bg="primary" color="light" px={10} borderRadius={0} mt={5}>
-            More about us
-        </Button>
-      </Box>
+            <Button bg="primary" color="light" px={10} py={7} borderRadius={0} mt={5}>
+                More about us
+            </Button>
+        </Box>
+
+        <Box px={10} my={10}>
+            <Text fontSize="32px" fontWeight="500" color="primary">Get started with us</Text>
+            
+        </Box>
     </>
   );
 }
